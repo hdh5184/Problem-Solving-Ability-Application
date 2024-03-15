00f755e8c8f4b9563bca8e187b1dab6fd73912aa
hdh5184 <hdh5184@gmail.com>
2024-03-15 21:34:44 +0900

    24.03.15 21:34 수정 및 중간 저장

--- 문제해결능력응용 2주차/Assets/script/Collection/Queue.cs
+++ 문제해결능력응용 2주차/Assets/script/Collection/Queue.cs
@@ -12,13 +12,14 @@ public class Queue : MonoBehaviour
 
     // 총알 Linked List
     class BulletLinkedList
-    {
-        Queue<Node> BulletQueue = new Queue<Node>();
-        Node queueBackNode;
+    { 
+        Queue<Node> BulletQueue = new Queue<Node>(); // 총알 노드 저장
+        Node queueBackNode; // Queue의 가장 뒤에 있는 요소
 
-        GameObject prefabObj;
-        int NodeCount;
+        GameObject prefabObj;   // 총알 Linked List 생성 시 지정할 프리펩 오브젝트
+        int NodeCount;          // 노드 카운트
 
+        // 생성자 및 초기화 (프리펩 오브젝, 추가할 노드 개수)
         public BulletLinkedList(GameObject prefab, int Count)
         {
             prefabObj = prefab;
@@ -31,33 +32,43 @@ public class Queue : MonoBehaviour
             
         }
 
+        // 노드 추가 및 Queue 넣기	
         void Enqueue(int Data)
         {
-            if(NodeCount == 0)
+            if(NodeCount == 0) // 처음 노드 생성하는 경우
             {
+                // Queue에 새로 생성한 (데이터 들어간) 노드 넣기
                 BulletQueue.Enqueue(new Node(Instantiate(prefabObj), Data));
-                queueBackNode = BulletQueue.Peek();
+
+                // 현재 노드가 1개만 있으니 Queue의 가장 뒤?에 있는 요소가 됨 어쨌든
+                queueBackNode = BulletQueue.Peek(); 
             }
-            else
+            else // 노드가 1개 이상이 있는 경우
             {
+                // 새로운 노드 생성 및 임시 저장
                 Node newNode = new Node(Instantiate(prefabObj), Data);
                 BulletQueue.Enqueue(newNode);
+
+                // 가장 나중에 넣은 노드를 새로 생성한 노드에 연결함
                 queueBackNode.Next = newNode;
+
+                // 새로 생성한 노드를 Queue의 가장 뒤?에 있는 요소로 지정
                 queueBackNode = newNode;
             }
 
-            NodeCount++;
+            NodeCount++; // 노드 개수 증가
         }
 
+        // 노드 빼기 및 Queue 빼기
         public void Dequeue()
         {
-            if (NodeCount != 0)
+            if (NodeCount != 0) // node가 남아 있으면
             {
                 GameObject dequeueBullet = BulletQueue.Dequeue().nodeBullet;
                 dequeueBullet.SetActive(true);
                 dequeueBullet.transform.position = new Vector3(-10, 1, 0);
             }
-            else return;
+            else return; //node가 남아 있지 않으면
 
             NodeCount--;
         }
@@ -77,11 +88,13 @@ public class Queue : MonoBehaviour
 
     class Node
     {
+        // 노드와 함께 생성한 오브젝트 연결
         public GameObject nodeBullet;
 
-        public Node Next;
-        public int Data;
+        public Node Next; // 다음 노드 연결
+        public int Data; // 데이터
 
+        // 연결할 오브젝트, 저장할 데이터 가져옴
         public Node(GameObject obj, int Num)
         {
             nodeBullet = obj;
@@ -91,9 +104,12 @@ public class Queue : MonoBehaviour
         }
     }
 
+    // 실행 시 BulletLinkedList 생성
     void Start()
     {
-        LinkedList = new BulletLinkedList(BulletPrefab, 10);
+        LinkedList = new BulletLinkedList(BulletPrefab, 10); // 10개
+
+        // 이건 노드 연결 여부를 확인할 디버그용
         LinkedList.DebugNode();
     }
 
